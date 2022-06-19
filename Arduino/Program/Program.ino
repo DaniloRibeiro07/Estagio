9e66c475a5193678966b1a59098ec57e2c2962a4
DaniloRibeiro07 <86561064+DaniloRibeiro07@users.noreply.github.com>
2022-06-19 01:21:26 -0300

    v3

---
+++ Arduino/Program/Program.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  static unsigned long tempo_ref=millis();
+  // put your main code here, to run repeatedly:
+  if ((millis()-tempo_ref)>2000){
+    static unsigned long cont;
+    Serial.print("Mensagem N°");
+    Serial.println(cont++);
+    tempo_ref=millis();
+  }
+  if (Serial.available()>0){
+    Serial.print(Serial.readString());
+    static bool estado=1;
+    digitalWrite(2, estado);
+    estado=!estado;
+  }
+  
+}
